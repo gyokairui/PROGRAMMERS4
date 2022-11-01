cea0f2047415c0fb736aef79deb4fba392385bbd
K.K <92494502+Koyamakenta@users.noreply.github.com>
2022-11-01 12:25:39 +0900

    koyama
    FUJINAGAkawauso

---
+++ dustbuster/Assets/Koyama/Scripts/Text/TimeE_S.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeE_S : MonoBehaviour
+{
+    public Text textUI;
+
+    void Start()
+    {
+        //コルーチンの実行
+        StartCoroutine("Hoge");
+    }
+
+    private IEnumerator Hoge()
+    {
+
+        textUI.text = "";
+
+        //数秒待つ
+        yield return new WaitForSeconds(7.5f);
+
+        //数秒待った後の処理
+        //textUI.text = "「W」：上";
+        textUI.text = "[S]：下";
+        //textUI.text = "「D」：右";
+        //textUI.text = "「A」：左";
+
+    }
+}
