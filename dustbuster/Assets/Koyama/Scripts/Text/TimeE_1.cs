6d4f2ff9f0e117cac14650334975049e66b959d3
K.K <92494502+Koyamakenta@users.noreply.github.com>
2022-11-11 13:26:43 +0900

    koyama
    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- dustbuster/Assets/Koyama/Scripts/Text/TimeE_1.cs
+++ dustbuster/Assets/Koyama/Scripts/Text/TimeE_1.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TimeE_S : MonoBehaviour
+public class TimeE_1 : MonoBehaviour
 {
     public Text textUI;
 
@@ -23,7 +23,7 @@ public class TimeE_S : MonoBehaviour
 
         //数秒待った後の処理
         //textUI.text = "「W」：上";
-        textUI.text = "[S]：下";
+        textUI.text = "W,S,A,D = 上,下,左,右";
         //textUI.text = "「D」：右";
         //textUI.text = "「A」：左";
 
--- dustbuster/Assets/Koyama/Scripts/Text/TimeE_2.cs
+++ dustbuster/Assets/Koyama/Scripts/Text/TimeE_2.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TimeE_D : MonoBehaviour
+public class TimeE_2 : MonoBehaviour
 {
     public Text textUI;
 
@@ -22,9 +22,9 @@ public class TimeE_D : MonoBehaviour
         yield return new WaitForSeconds(7.5f);
 
         //数秒待った後の処理
-        //textUI.text = "「W」：上";
+        textUI.text = "ゴミを集めてゴミ箱に捨てよう！";
         //textUI.text = "「S」：下";
-        textUI.text = "[D]：右";
+        //textUI.text = "「D」：右";
         //textUI.text = "「A」：左";
 
     }
