5c5cef3402b77718c05cd65d5634ad848ae5f4e9
Nakanoshintarou <h1202119026@hiratagakuen.onmicrosoft.com>
2022-12-07 12:31:03 +0900

    nakanoj

---
+++ dustbuster/Assets/Gagecollar.cs
@@ -0,0 +1,21 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class Gagecollar : MonoBehaviour
+//{
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        GetComponent<Renderer>().material.color = Color.red;
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        if (Player.DustFULL == true)
+//        {
+            
+//        }
+//    }
+//}
